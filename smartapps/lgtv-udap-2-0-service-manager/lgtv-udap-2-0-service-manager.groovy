df041bf73ff489ae30bdaac067a24a38c478d5e5
Tobias Haerke <tosty@outlook.com>
2017-06-10 18:13:09 +0200

    Pairing fully implemented, polished interface

--- smartapps/lgtv-udap-2-0-service-manager/lgtv-udap-2-0-service-manager.groovy
+++ smartapps/lgtv-udap-2-0-service-manager/lgtv-udap-2-0-service-manager.groovy
@@ -25,12 +25,9 @@ definition(
 
 
 preferences {
-	page(name: "searchTargetSelection", title: "UPnP Search Target", nextPage: "deviceDiscovery") {
-		section("Search Target") {
-			input "searchTarget", "string", title: "Search Target", defaultValue: "udap:rootservice", required: true
-		}
-	}
-	page(name: "deviceDiscovery", title: "UPnP Device Setup", content: "deviceDiscovery")
+    page(name: "mainPage")
+	page(name: "deviceDiscovery")
+    page(name: "devicePairing")
 }
 
 def installed() {
@@ -57,7 +54,25 @@ def initialize() {
 		addDevices()
 	}
 
-	runEvery5Minutes("ssdpDiscover")
+	//runEvery5Minutes("ssdpDiscover")
+}
+
+def mainPage() {
+    state.searchTarget = "udap:rootservice"
+    return dynamicPage(name: "deviceDiscovery", title: "LG TV service manager", nextPage: "", uninstall: true) {
+        section("Discover devices"){
+           href "deviceDiscovery", title: "Discover devices", description:""
+        }
+        section("Installed devices"){
+          	def dMap = [:]
+            getChildDevices().sort({ a, b -> a["label"] <=> b["label"] }).each {
+            	paragraph "$it.label"
+            }
+        }
+        /*section("Search Target") {
+            input "searchTarget", "string", title: "Search Target", defaultValue: "udap:rootservice", required: true
+        }*/
+ 	}
 }
 
 def deviceDiscovery() {
@@ -74,7 +89,7 @@ def deviceDiscovery() {
 	ssdpDiscover()
 	verifyDevices()
 
-	return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
+	return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", refreshInterval: 5, nextPage: "devicePairing") {
 		section("Please wait while we discover your UPnP Device. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
 			input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
 		}
@@ -93,7 +108,7 @@ def getDevices() {
 }
 
 void ssdpSubscribe() {
-	subscribe(location, "ssdpTerm.${searchTarget}", ssdpHandler)
+	subscribe(location, "ssdpTerm.${state.searchTarget}", ssdpHandler)
 }
 
 def ssdpHandler(evt) {
@@ -126,13 +141,13 @@ def ssdpHandler(evt) {
 			}
 		}
 	} else {
-    	log.debug "ssdpHandler, device doesnot: " + ssdpUSN
+    	log.debug "ssdpHandler, device doesnot exist: " + ssdpUSN
 		devices << ["${ssdpUSN}": parsedEvent]
 	}
 }
 
 void ssdpDiscover() {
-	sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${searchTarget}", physicalgraph.device.Protocol.LAN))
+	sendHubCommand(new physicalgraph.device.HubAction("lan discovery ${state.searchTarget}", physicalgraph.device.Protocol.LAN))
 }
 
 void verifyDevices() {
@@ -146,7 +161,6 @@ void verifyDevices() {
         log.info "verifyDevices host: " + host + " path: " + path
         
 		sendHubCommand(new physicalgraph.device.HubAction("""GET ${path} HTTP/1.1\r\nHOST: $host\r\nUser-Agent: UDAP/2.0 Samsung Smartthings \r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
-    	//sendHubCommand(new physicalgraph.device.HubAction("""GET ${path} HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: hubResponseReceived]))
     }
 }
 
@@ -163,13 +177,13 @@ void deviceDescriptionHandler(physicalgraph.device.HubResponse hubResponse) {
     
     def ssdpUSN = "uuid:"+uuid+"::udap:rootservice"
 
-    log.error "modelName: " + modelName + ", friendlyName: " + friendlyName + ", uuid: " + uuid + ", ssdpUSN: " + ssdpUSN
+    log.error "modelName: " + modelName + ", friendlyName: " + friendlyName + ", uuid: " + uuid + ", ssdpUSN: " + ssdpUSN + ", pairingKey: " + null +", paired: " + false
 	
     def devices = getDevices()
 	def device = devices.find { it?.key?.contains( ssdpUSN ) }
 	if (device) {
     	log.debug "Verifying device: " + ssdpUSN
-		device.value << [name: friendlyName, model: modelName, serialNumber: uuid, verified: true]
+		device.value << [name: friendlyName, model: modelName, serialNumber: uuid, pairingKey:null, paired:false, verified: true]
 	}
 }
 
@@ -198,21 +212,103 @@ def addDevices() {
 		}
 
 		if (!d) {
-        	log.debug "Creating LGTV UDAP 2.0 Device with name: " + selectedDevice.value.name + ", mac: " + selectedDevice.value.mac + ", ip: " + selectedDevice.value.networkAddress + ", port: " + selectedDevice.value.deviceAddress + ", netcruUrl: " + selectedDevice.value.ssdpPath
+        	log.warn "device " + dni + " doesnot exist, selectedDevice: " + selectedDevice
+            
+            if(settings["pairingKey"+selectedDevice.value.serialNumber]) {
+            	log.warn "Pairing key exists " + settings["pairingKey"+selectedDevice.value.serialNumber]
+            	if(selectedDevice.value.paired == true) {
+                	log.warn "Device paired "
+                    log.debug "Creating LGTV UDAP 2.0 Device with name: " + selectedDevice.value.name + ", mac: " + selectedDevice.value.mac + ", ip: " + selectedDevice.value.networkAddress + ", port: " + selectedDevice.value.deviceAddress + ", netcruUrl: " + selectedDevice.value.ssdpPath + ", uuid: " + selectedDevice.value.serialNumber + ", pairingKey: " + settings["pairingKey"+selectedDevice.value.serialNumber]
         	
-			addChildDevice("vervallsweg", "LGTV UDAP 2.0", selectedDevice.value.mac, selectedDevice?.value.hub, [
-				"label": selectedDevice?.value?.name ?: "LGTV UDAP 2.0",
-				"data": [
-					"mac": selectedDevice.value.mac,
-					"ip": selectedDevice.value.networkAddress,
-					"port": selectedDevice.value.deviceAddress,
-                    "netcruUrl": selectedDevice.value.ssdpPath,
-                    "pairingKey": "123456"
-				]
-			])
-            log.warn "device " + dni + " doesnot exist, selectedDevice: " + selectedDevice
+                    addChildDevice("vervallsweg", "LGTV UDAP 2.0", selectedDevice.value.mac, selectedDevice?.value.hub, [
+                        "label": selectedDevice?.value?.name ?: "LGTV UDAP 2.0",
+                        "data": [
+                            "mac": selectedDevice.value.mac,
+                            "ip": selectedDevice.value.networkAddress,
+                            "port": selectedDevice.value.deviceAddress,
+                            "netcruUrl": selectedDevice.value.ssdpPath,
+                            "uuid": selectedDevice.value.serialNumber,
+                            "model": selectedDevice.value.model,
+                            "pairingKey": settings["pairingKey"+selectedDevice.value.serialNumber]
+                        ]
+                    ])
+                } else {
+                	log.warn "Device not paired"
+                    pairDevice(dni, settings["pairingKey"+selectedDevice.value.serialNumber])
+                }
+            }
 		} else {
         	log.warn "device " + dni + " exists"
         }
 	}
+}
+
+def devicePairing() {
+	def devices = getDevices()
+    def pairingKeyMap = [:]
+
+	log.debug "devicePairing, devices: " + devices
+    log.debug "devicePairing, selectedDevices: " + selectedDevices
+    
+    return dynamicPage(name: "devicePairing", title: "Enter pairing key", nextPage: "", install: true, uninstall: true) {
+		section("A pairing key will be displayed on every newly added device. Go to your TVs now and enter every key below. Note: The keys will expire after some time. If you don't finish installation now you maybe need to restart the process.") {
+			selectedDevices.each { dni ->
+            	def selectedDevice = devices.find { it.value.mac == dni }
+                if(!selectedDevice.value.pairingKey) {
+                	int port = convertHexToInt(selectedDevice.value.deviceAddress)
+                    String ip = convertHexToIP(selectedDevice.value.networkAddress)
+                    String host = "${ip}:${port}"
+                	requestPairingKey( host ) 
+               	}
+                
+                input "pairingKey"+selectedDevice.value.serialNumber, "number", required: true, title: selectedDevice.value.model+" ("+selectedDevice.value.name+")", defaultValue: selectedDevice.value.pairingKey
+            }
+		}
+	}
+}
+
+def requestPairingKey(host) {
+	log.error "requestPairingKey, host: " + host + "/udap/api/pairing"
+    def hubComm = new physicalgraph.device.HubAction("""POST /udap/api/pairing HTTP/1.1\r\nHOST: $host\r\nUser-Agent: UDAP/2.0 Samsung Smartthings\r\nContent-Type: text/xml; charset=utf-8\r\nCache-Control: no-cache\r\nContent-Length: 105\r\nConnection: Close\r\n\r\n<?xml version="1.0" encoding="utf-8"?><envelope><api type="pairing"><name>showKey</name></api></envelope>""", physicalgraph.device.Protocol.LAN, host, [callback: requestPairingKeyHandler])
+    sendHubCommand(hubComm)
+    log.warn "hubComm: " + hubComm
+}
+
+def requestPairingKeyHandler(physicalgraph.device.HubResponse hubResponse) {
+	log.debug 'requestPairingKeyHandler, hubResponse.status: ' + hubResponse.status
+}
+
+def pairDevice(dni, pairingKey) {
+	log.error "pairDevice, dni: " + dni + ", pairingKey: " + pairingKey
+    
+    def selectedDevice = devices.find { it.value.mac == dni }
+    int port = convertHexToInt(selectedDevice.value.deviceAddress)
+    String ip = convertHexToIP(selectedDevice.value.networkAddress)
+    String host = "${ip}:${port}"
+    selectedDevice.value.pairingKey = pairingKey
+    log.warn "selectedDevice: " + selectedDevice
+    
+    def hubComm = new physicalgraph.device.HubAction("""POST /udap/api/pairing HTTP/1.1\r\nHOST: $host\r\nUser-Agent: UDAP/2.0 Samsung Smartthings\r\nContent-Type: text/xml; charset=utf-8\r\nCache-Control: no-cache\r\nContent-Length: 141\r\nConnection: Close\r\n\r\n<?xml version="1.0" encoding="utf-8"?><envelope><api type="pairing"><name>hello</name><value>${pairingKey}</value><port>8080</port></api></envelope>""", physicalgraph.device.Protocol.LAN, host, [callback: pairDeviceHandler])
+    sendHubCommand(hubComm)
+    log.warn "hubComm: " + hubComm
+}
+
+def pairDeviceHandler(physicalgraph.device.HubResponse hubResponse) {
+    log.debug 'pairDeviceHandler, hubResponse.mac: ' + hubResponse.mac + ', hubResponse.status: ' + hubResponse.status
+    def selectedDevice = devices.find { it.value.mac == hubResponse.mac }
+    
+    //RESPONSE,REQ MATCHING > currDevi.paired==true
+    if (selectedDevice) {
+    	log.debug "Device found, selectedDevice: " + selectedDevice
+        
+        if(hubResponse.status==200){
+            log.info "Successfully paired device, selectedDevice.value.deviceAddress: " + selectedDevice.value.deviceAddress
+            selectedDevice.value.paired=true
+        } else {
+            log.error "Could not pair device, selectedDevice.value.deviceAddress: " + selectedDevice.value.deviceAddress + ", hubResponse.status: " + hubResponse.status
+        }
+    } else {
+    	log.warn "Pairing response received from non ST device, hubResponse.mac: " + hubResponse.mac
+    }
+    
 }
