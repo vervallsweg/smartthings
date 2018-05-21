8b6adea371c137d99285f338bcd51bfe2e8707b4
Tobias Haerke <tosty@outlook.com>
2018-05-21 15:54:44 +0200

    Removed -1 from names

--- devicetypes/vervallsweg/cast-web-api.src/cast-web-api.groovy
+++ devicetypes/vervallsweg/cast-web-api.src/cast-web-api.groovy
@@ -14,25 +14,25 @@
  *
  */
  preferences {
-	input("configLoglevel", "enum", title: "Log level?",
+    input("configLoglevel", "enum", title: "Log level?",
         required: false, multiple:false, value: "nothing", options: ["0","1","2","3","4"])
 }
  
 metadata {
-	definition (name: "cast-web-api", namespace: "vervallsweg", author: "Tobias Haerke") {
-		capability "Actuator"
-		capability "Bridge"
+    definition (name: "cast-web-api", namespace: "vervallsweg", author: "Tobias Haerke") {
+        capability "Actuator"
+        capability "Bridge"
         capability "Refresh"
         
         command "checkVersion"
-	}
+    }
 
-	simulator {
-		// TODO: define status and reply messages here
-	}
+    simulator {
+        // TODO: define status and reply messages here
+    }
 
-	tiles(scale: 2) {
-		standardTile("mainTile", "device", width: 6, height: 2, decoration: "flat") {
+    tiles(scale: 2) {
+        standardTile("mainTile", "device", width: 6, height: 2, decoration: "flat") {
             state "val", icon:"https://raw.githubusercontent.com/vervallsweg/smartthings/master/icn/ic-cast-web-api-gray-1200.png", label:'Active', action:null, defaultState: true
         }
         standardTile("refresh", "device.refresh", width: 2, height: 2, decoration: "flat") {
@@ -43,67 +43,67 @@ metadata {
         }
         //main "mainTile"
         details(["mainTile", "refresh", "updateStatus"])
-	}
+    }
 }
 
 // parse events into attributes
 def parse(String description) {
-	//logger('debug', "Parsing: "+description)
+    //logger('debug', "Parsing: "+description)
     def message = parseLanMessage(description)
-   	def children = getChildDevices()
+    def children = getChildDevices()
     logger('debug', "Parsing json: "+message.json)
     
     if(message.json) {
-    	if(message.json.id) {
-        	def foundTarget = false;
-			children.each { child ->
+        if(message.json.id) {
+            def foundTarget = false;
+            children.each { child ->
                 if ( child.deviceNetworkId == message.json.id ){
-                	foundTarget = true;
+                    foundTarget = true;
                     logger('debug', "Parsing found target name: "+child.displayName+", dni: "+child.deviceNetworkId)
                     if(message.json.name) {
                         child.displayName = message.json.name
                     }
                     //if(message.json.connection) { //WTF: cannot be set by dev?!
                     //    if( message.json.connection.equals("connected") ) {
-                    //    	child.status = "ONLINE"
+                    //      child.status = "ONLINE"
                     //    }
                     //    if( message.json.connection.equals("disconnected") ) {
-                    //    	child.status = "OFFLINE"
+                    //      child.status = "OFFLINE"
                     //    }
                     //}
                     child.parse(description)
                 }
             }
             if(!foundTarget) {
-				logger('error', "Parsing found no target for: "+message.json.id)
- 			}
+                logger('error', "Parsing found no target for: "+message.json.id)
+            }
         } else {
-        	def vthis, vlatest
+            def vthis, vlatest
             if (message.json.this) {
-            	vthis = message.json.this
+                vthis = message.json.this
             }
             if (message.json.latest) {
-            	vlatest = message.json.latest
+                vlatest = message.json.latest
             }
             if (vthis && vlatest) {
-            	sendEvent(name: "updateStatus", value: ("Current: "+ vthis + "\nLatest: " + vlatest), displayed: false)
+                sendEvent(name: "updateStatus", value: ("Current: "+ vthis + "\nLatest: " + vlatest), displayed: false)
             }
         }
     }
 }
 
 def installed() {
-	installDevices( parseListFromString( getDataValue("devices") ) )
+    installDevices( parseListFromString( getDataValue("devices") ) )
     sendEvent(name: "updateStatus", value: "Click to check for updates", displayed: false)
 }
 
 def updated() {
-	installDevices( parseListFromString( getDataValue("devices") ) )
+    installDevices( parseListFromString( getDataValue("devices") ) )
 }
 
 def refresh() {
-	getChildDevices().each { child ->
-		child.refresh()
+    getChildDevices().each { child ->
+        child.refresh()
     }
 }
 
@@ -113,19 +113,19 @@ def installDevices(ids) {
     logger('debug', "installDevices() children: " + children.size() + ", ids: " + ids)
     
     ids.each { id ->
-   		
+        
         def exists = false
         
         children.each { child ->
-        	if( id.equals( child.deviceNetworkId ) ) { //id has a nbsp added to the front
-            	exists = true
+            if( id.equals( child.deviceNetworkId ) ) { //id has a nbsp added to the front
+                exists = true
             }
-      	}
+        }
         
         if (exists) {
-        	logger('debug', "installDevices() id: "+id+", exists!")
+            logger('debug', "installDevices() id: "+id+", exists!")
         } else {
-        	logger('warn', "installDevices() id: "+id+", doesn't exist")
+            logger('warn', "installDevices() id: "+id+", doesn't exist")
             devicesToCreate.add(id)
             logger('debug', "installDevices() devicesToCreate: "+devicesToCreate)
         }
@@ -135,12 +135,12 @@ def installDevices(ids) {
 }
 
 def createDevices(ids) {
-	logger('debug', "createDevices() ids: " + ids)
+    logger('debug', "createDevices() ids: " + ids)
     
     ids.each {
         if(it!=null&&it!="") {
-        	logger('debug', "createDevices() adding id: " + it)
-            addChildDevice("vervallsweg", "cast-web-1", it, location.hubs[0].id, [
+            logger('debug', "createDevices() adding id: " + it)
+            addChildDevice("vervallsweg", "cast-web", it, location.hubs[0].id, [
                 label: "cast-web-device",
                 data: [
                     "apiHost": getDataValue("apiHost")
@@ -158,8 +158,8 @@ def parseListFromString(string) {
 }
 
 def checkVersion() {
-	def host = getDataValue("apiHost")
-	sendHubCommand(new physicalgraph.device.HubAction("""GET /config/version HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host))
+    def host = getDataValue("apiHost")
+    sendHubCommand(new physicalgraph.device.HubAction("""GET /config/version HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host))
 }
 
 
